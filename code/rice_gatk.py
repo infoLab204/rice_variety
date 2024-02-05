f60fef09e94841c89a6a28a1e3093974b29deff5
infoLab204 <63629577+infoLab204@users.noreply.github.com>
2024-02-05 13:36:29 +0900

    Update rice_gatk.py

--- code/rice_gatk.py
+++ code/rice_gatk.py
@@ -4,14 +4,14 @@ import sys
 
 ## The program and working directory must be set.
 
-##program setting
+## program setting
 BWA="bwa-mem2-2.2.1/"
 SAMTOOLS="samtools-1.17/"
 PICARD="picard.jar"
 GATK="gatk-package-4.3.0.0-local.jar"
 
 
-## reference
+## reference sequence
 ref="IRGSP-1.0_genome.fasta "
 
 ## working dictionary setting
@@ -41,13 +41,13 @@ print(f"rm -rf {root_dir}{alignment}/temp/{samp_name}.fixmated.bam")
 print(f"{SAMTOOLS}samtools index  {root_dir}{alignment}/temp/{samp_name}.fixmated.sored.bam")
 
 ## Mark Duplicate : picard tools
-print(f"jar -jar {PICARD} MarkDuplicates I={root_dir}{alignment}/temp/{samp_name}.fixmated.sored.bam O={root_dir}{alignment}/{samp_name}_aligned.bam M={root_dir}{alignment}/temp/{samp_name}.markdup.metrics.txt")
+print(f"java -jar {PICARD} MarkDuplicates I={root_dir}{alignment}/temp/{samp_name}.fixmated.sored.bam O={root_dir}{alignment}/{samp_name}_aligned.bam M={root_dir}{alignment}/temp/{samp_name}.markdup.metrics.txt")
 print(f"rm -rf  {root_dir}{alignment}/temp/{samp_name}.fixmated.sored.bam ")
  
 ## Samtools index : Make BAM index
 print(f"{SAMTOOLS}samtools index {root_dir}{alignment}/{samp_name}_aligned.bam")
 
-### GATK BaseRecalibrator
+## GATK BaseRecalibrator
 print(f"java -jar {GATK} BaseRecalibrator -R {ref} -I {root_dir}{alignment}/{samp_name}_aligned.bam  --known-sites {root_dir}/db/oryza_sativa_dbSNP_20230103.vcf.gz  -O {root_dir}{recalibration}/temp/{samp_name}_recalibrated.table")
 print(f"java -jar {GATK} ApplyBQSR  -R {ref} -I {root_dir}{alignment}/{samp_name}_aligned.bam --bqsr-recal-file {root_dir}{recalibration}/temp/{samp_name}_recalibrated.table -O {root_dir}{recalibration}/{samp_name}_recalibrated.bam")
    
